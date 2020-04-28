b94f92716613075aa2564540ff7723b1d17283d0
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2020-04-28 18:51:32 -0400

    Change map fig to use tile-based instead of outline-based maps
    Switch from px.choropleth to px.choropleth_mapbox for better mobile support.

--- template.py
+++ template.py
@@ -68,13 +68,19 @@ def generate_layout(labels):
     # get max cases value
     cases_max_val = cases_per1000_long['cases_per_1000'].max()
     # Montreal cases per 1000 map
-    mtlmap_fig = px.choropleth(cases_per1000_long, geojson=mtl_geojson, locations='borough', color='cases_per_1000',
-                        featureidkey="properties.borough", animation_frame='date', range_color=[0, cases_max_val],
-                        projection="winkel tripel", labels=labels['montreal_map_colourbar_labels'])
-    mtlmap_fig.update_geos(fitbounds="locations", visible=False)
-    mtlmap_fig.layout.sliders[0]['pad'] = {'b': 10, 't': 0}
+    mtlmap_fig = px.choropleth_mapbox(cases_per1000_long, geojson=mtl_geojson, locations='borough', color='cases_per_1000',
+                    featureidkey="properties.borough", animation_frame='date', animation_group='borough',
+                    mapbox_style='carto-positron', range_color=[0, cases_max_val],
+                    zoom=9, center = {'lat': 45.55, 'lon': -73.75},
+                    labels=labels['montreal_map_colourbar_labels'])
+
     mtlmap_fig.layout.sliders[0]['currentvalue']['prefix'] = labels['date_slider_label']
-    mtlmap_fig.layout.updatemenus[0]['pad'] = {'r': 10, 't': 10}
+    mtlmap_fig.layout.sliders[0]['xanchor'] = 'left'
+    mtlmap_fig.layout.sliders[0]['pad'] = {'b': 10, 't': 0, 'l': 50}
+    mtlmap_fig.layout.updatemenus[0]['xanchor'] = 'left'
+    mtlmap_fig.layout.updatemenus[0]['x'] = 0
+    mtlmap_fig.layout.updatemenus[0]['pad'] = {'r': 50, 't': 15}
+
     mtlmap_fig.update_layout({
         'margin': {"r":0,"t":0,"l":0,"b":0},
         'plot_bgcolor': 'rgba(0,0,0,0)',
@@ -467,7 +473,7 @@ def generate_layout(labels):
                                                     'lasso2d',
                                                     'hoverClosestGeo',
                                                     ],
-                                                'scrollZoom' : False
+                                                'scrollZoom' : True
                                                 }
                                             ),
 
