885ef5345f13205f65069d23d5f7876bd0b93354
Yannick Gingras <ygingras@ygingras.net>
2020-05-02 19:22:53 -0700

    adding scraper CLI option for DATA_DIR

--- app/refreshdata.py
+++ app/refreshdata.py
@@ -20,14 +20,14 @@ SOURCES = { #'cases.csv': '',
 
             # The main page for this dataset is https://www.inspq.qc.ca/covid-19/donnees 
             'data_qc.csv': 'https://www.inspq.qc.ca/sites/default/files/covid/donnees/combine.csv'}
-DATA_DIR = '/tmp/data'
+DATA_DIR = 'data'
 NB_RETRIES = 3
 
-def lock():
+def lock(lock_dir):
     ''' Lock the data directory to prenvent concurent runs of the scraper, 
     which would be risky for data corruption. '''
 
-    lockf = os.path.join(DATA_DIR, 'scraper.pid')
+    lockf = os.path.join(lock_dir, 'scraper.pid')
     if not os.path.isfile(lockf):
         open(lockf, 'w') # create the empty file
     fd = os.open(lockf, os.O_RDONLY)
@@ -51,28 +51,29 @@ def fetch(url):
 def save_df(filename, data):
     ''' Save a datafile if it's newer and at least as big as what we cached.  
     Raise ValueError otherwise.'''
-    fq_path = os.path.join(DATA_DIR, 'sources', filename)
-    if os.path.isfile(fq_path):
+    if os.path.isfile(filename):
         # TODO: double check independent of the encoding
-        if os.path.getsize(fq_path) > len(data):
+        if os.path.getsize(filename) > len(data):
             msg = '{} is smaller than the cached version we have on disk.'.format(filename)
             raise ValueError(msg)
         # backup the old file
         time_tag = datetime.utcnow().isoformat()
-        base, ext = os.path.splitext(fq_path) 
+        base, ext = os.path.splitext(filename) 
         os.link(fq_path, '{}-{}{}'.format(base, time_tag, ext))
     # it's all good if we made it this far, save the new file 
     open(fq_path, 'wb').write(data)
 
 def main():
-    parser = ArgumentParser("refreshdata", description=__doc__)
+    parser = ArgumentParser('refreshdata', description=__doc__)
+    data_dir = os.path.join(os.path.dirname(__file__), DATA_DIR)
+    parser.add_argument('-d', '--data-dir', default=data_dir)
     args = parser.parse_args()
 
-    lock()
+    lock(args.data_dir)
 
     for file, url in SOURCES.items():
         data = fetch(url)
-        save_df(file, data)
+        save_df(os.path.join(args.data_dir, 'sources', file), data)
 
     return 0
 
