ce91c044bbb43b122f406806dc77a8d51e08405e
Matthias Schoettle <git@mattsch.com>
2020-12-29 13:35:52 -0500

    Fixes problem with erroneous rows in data_qc.csv.

--- refreshdata.py
+++ refreshdata.py
@@ -236,9 +236,12 @@ def load_data_qc_csv(source_file):
     source_file : str
         Absolute path of source file.
     """
-    qc_df = pd.read_csv(source_file, encoding='utf-8', na_values=['', '         .  '])
+    qc_df = pd.read_csv(source_file, encoding='utf-8', na_values=['', '         .  ', '           .'])
     # cut off first rows with 'Date inconnue'
     qc_df = qc_df[qc_df['Date'] != 'Date inconnue']
+    # sometimes extra rows for INC, Inconnue show up with '           .' in the column 'cas_cum_tot_n'
+    # considering the occurring string as NaN and drop those rows (otherwise cannot be converted to int later)
+    qc_df.dropna(subset=['cas_cum_tot_n'], inplace=True)
 
     column_mappings = {
         'Date': 'date',
