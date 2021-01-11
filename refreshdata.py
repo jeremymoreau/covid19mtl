c6fb39a947d122571a62ce25b78ff3a89a4100fa
Matthias Schoettle <git@mattsch.com>
2021-01-10 21:46:52 -0500

    Transposes cases.csv so it's easier to see the diff.

--- refreshdata.py
+++ refreshdata.py
@@ -403,28 +403,25 @@ def append_mtl_cases_csv(sources_dir, processed_dir, date):
     # Remove last row (total count)
     new_data_col = new_data_col[:-1]
 
-    # check if column already exists in cases_csv, append column if it doesn't
-    if cases_df.columns[-1] == date:
+    # check if date already exists in cases_csv, append if it doesn't
+    if date in cases_df.index:
         print(f'MTL cases: {date} has already been appended to {cases_csv}')
     else:
-        # Append new col of data
-        cases_df[date] = list(new_data_col)
-
-        # check whether the exact same data already exists for a previous day
+        # check whether this is actually new data and not data from a previous day (see issue #34)
         # data is not updated on Sat (for Fri) and Sun (for Sat) and still shows previous days data
         # replace it with 'na' if it is the same
-        # see: issue #34
         already_exists = False
-        # need to go back several columns in case the previous one has 'na'
-        # TODO: can just check the previous two rows if na values are dropped (see append_mtl_age_csv)
-        for i in range(len(cases_df.columns) - 1, 1, -1):
-            if pd.Series.all(cases_df[date] == cases_df[cases_df.columns[i - 1]]):
-                already_exists = True
-                break
+        # check with last row that has actual values
+        print(cases_df.dropna().iloc[-1])
+        if pd.Series.all(cases_df.dropna().iloc[-1] == list(new_data_col)):
+            already_exists = True
+
+        # Append new row of data
+        cases_df.loc[date] = list(new_data_col)
 
         if already_exists:
             print(f'MTL cases: the data shown on {date} already exists for a previous day, replacing with "na"')
-            cases_df[cases_df.columns[-1]] = 'na'
+            cases_df.iloc[-1] = 'na'
 
     # Overwrite cases.csv
     cases_df.to_csv(cases_csv, encoding='utf-8', na_rep='na')
@@ -443,11 +440,10 @@ def update_mtl_boroughs_csv(processed_dir):
     cases_df = pd.read_csv(cases_csv, index_col=0, encoding='utf-8', na_values='na', parse_dates=True)
 
     # drop to be confirmed (TBC) cases
-    cases_df = cases_df[:-1]
+    cases_df = cases_df.iloc[:, :-1]
 
-    # make date the index
     # and drop rows with only NA
-    cases_df = cases_df.transpose().dropna(how='all').astype(int)
+    cases_df = cases_df.dropna(how='all').astype(int)
     # convert index to DateTimeIndex
     cases_df.index = pd.to_datetime(cases_df.index)
 
