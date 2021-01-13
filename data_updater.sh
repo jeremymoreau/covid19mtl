7a9e1bce8170ce395f5380a72ebddc1b4834c305
Matthias Schoettle <git@mattsch.com>
2021-01-13 10:23:14 -0500

    Improves data updater script.
    Removes verbose output, only prints output if new data is found.
    Only attempts to continue further if local changes detected.
    Only attempts to commit and push if changes are staged on index.
    Adds output from refreshdata to commit message.

--- data_updater.sh
+++ data_updater.sh
@@ -11,41 +11,53 @@ cd $SCRIPT_DIR
 source .venv/bin/activate
 
 # pull in case there were commits
-echo "pulling latest state of repo..."
+# echo "pulling latest state of repo..."
 git pull
 
 # download sources, backup current data and update processed CSVs
-echo "downloading data, processing files..."
-python refreshdata.py
+# echo "downloading data, processing files..."
+OUTPUT=$(python refreshdata.py)
 
-# create version tag, increment if it already exists
-echo "determining version tag..."
-TODAY=$(date +%Y.%m.%d)
-VERSION_TAG="v$TODAY.0"
-REMOTE_TAGS=$(git ls-remote --tags origin)
+# check if there are local changes
+# see: https://stackoverflow.com/q/5143795
+if ! git diff-index --quiet HEAD --; then
+  echo "detected local changes:"
+  echo "$OUTPUT"
 
-while [[ $REMOTE_TAGS == *$VERSION_TAG* ]]; do
-  i="$((${VERSION_TAG: -1}+1))"
-  VERSION_TAG="v$TODAY.$i"
-done
+  # create version tag, increment if it already exists
+  echo "determining version tag..."
+  TODAY=$(date +%Y.%m.%d)
+  VERSION_TAG="v$TODAY.0"
+  REMOTE_TAGS=$(git ls-remote --tags origin)
 
-echo "committing and pushing changes..."
+  while [[ $REMOTE_TAGS == *$VERSION_TAG* ]]; do
+    i="$((${VERSION_TAG: -1}+1))"
+    VERSION_TAG="v$TODAY.$i"
+  done
 
-# support Mac/BSD date and Linux/GNU date
-if [ "$(uname)" == "Darwin" ]; then
-    YESTERDAY=$(date -v-1d +"%Y-%m-%d")
-else
-    YESTERDAY=$(date +%Y-%m-%d -d "1 day ago")
+  echo "version tag: $VERSION_TAG"
+
+  echo "committing and pushing changes..."
+
+  # support Mac/BSD date and Linux/GNU date
+  if [ "$(uname)" == "Darwin" ]; then
+      YESTERDAY=$(date -v-1d +"%Y-%m-%d")
+  else
+      YESTERDAY=$(date +%Y-%m-%d -d "1 day ago")
+  fi
+
+  # add updated files to index
+  git add $DATA_DIR/processed/*.csv > /dev/null
+  git add $DATA_DIR/processed_backups/$(date +%Y-%m-%d)/*.csv > /dev/null
+  git add $DATA_DIR/sources/$YESTERDAY/* > /dev/null
+
+  # check if there are now changes on the index (staged for commit)
+  if ! git diff-index --cached --quiet HEAD --; then
+    # commit, tag and push
+    git commit -m "Automatic update: Data update for $YESTERDAY" -m "$OUTPUT"
+    git push
+
+    git tag $VERSION_TAG
+    git push --tags
+  fi
 fi
-
-# add updated files to index
-git add $DATA_DIR/processed/*.csv
-git add $DATA_DIR/processed_backups/*.csv
-git add $DATA_DIR/sources/$YESTERDAY*
-
-# commit, tag and push
-git commit -m "Automatic update: Data update for $YESTERDAY"
-git push
-
-git tag $VERSION_TAG
-git push --tags
